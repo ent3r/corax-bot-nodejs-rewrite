0302ac293033f068dad09f2f7b569e1dec4e6f17
ent3r <32072697+ent3r@users.noreply.github.com>
2020-08-24 20:54:18 +0200

    feat: Add words command
    Counts how many words there are in the input string

--- app/commands/utility.ts
+++ app/commands/utility.ts
@@ -18,6 +18,21 @@ const commands = new CommandGroup([
       message.channel.send(args.join(" ").split("").reverse().join(""));
     }
   ),
+  new Command(
+    {
+      name: "words",
+      aliases: ["wordcount", "countwords"],
+      help: {
+        description: "Counts the amount of words in a message",
+        category: "Utility",
+        usage: "<string>",
+        arguments: [{ name: "string", required: true }],
+      },
+    },
+    (client: Client, message: Message, args: Array<string>): void => {
+      message.channel.send(args.join(" ").split(" ").length);
+    }
+  ),
 ]);
 
 module.exports = commands;
