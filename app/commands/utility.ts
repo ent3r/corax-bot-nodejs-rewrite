cbf065652c8536322d79eaa1c51d05758635812c
ent3r <32072697+ent3r@users.noreply.github.com>
2020-08-24 22:48:02 +0200

    refactor: Move ping command into utility file

--- app/commands/utility.ts
+++ app/commands/utility.ts
@@ -1,7 +1,7 @@
 import CommandGroup from "../typings/CommandGroup";
 import Command from "../typings/Command";
 
-import { Client, Message } from "discord.js";
+import { Client, Message, MessageEmbed } from "discord.js";
 
 const commands = new CommandGroup([
   new Command(
@@ -31,6 +31,52 @@ const commands = new CommandGroup([
     },
     (client: Client, message: Message, args: Array<string>): void => {
       message.channel.send(args.join(" ").split(" ").length);
+    }
+  ),
+  new Command(
+    {
+      name: "ping",
+      aliases: ["delay"],
+      cooldown: 5,
+      help: {
+        description: "Displays the ping to discord",
+        category: "Utility",
+        usage: "",
+      },
+    },
+    async (
+      client: Client,
+      message: Message,
+      _args: Array<string>
+    ): Promise<void> => {
+      //? Create a new embed. This is the embed that will be edited, and it is therefore saved
+      const embed = new MessageEmbed({
+        title: "Pinging...",
+      }).setColor("#FFCC00");
+
+      //? Send the embed, and with the returned message object
+      message.channel.send(embed).then((m) => {
+        //? Change the embed to contain the correct values
+        embed
+          .setTitle("Pong!")
+          .setColor("#00FF00")
+          .setFooter(message.author.username, message.author.displayAvatarURL())
+          .setTimestamp(new Date())
+          .addField(
+            "Round trip latency",
+            `${Math.floor(
+              m.createdAt.getTime() - message.createdAt.getTime()
+            )}ms`
+          )
+          .addField(
+            "Discord websocket ping",
+            `${Math.round(client.ws.ping)}ms`
+          );
+        //? And then edit the message we sent with this new embed
+        m.edit(embed);
+      });
+      //? Return because why not
+      return;
     }
   ),
 ]);
