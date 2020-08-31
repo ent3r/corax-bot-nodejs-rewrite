7892c857a1c9883e2ff003bc37044d07c782e483
ent3r <32072697+ent3r@users.noreply.github.com>
2020-08-31 20:58:31 +0200

    :sparkles: Add a help command

--- app/commands/help.ts
+++ app/commands/help.ts
@@ -2,7 +2,15 @@ import { Client, Message } from "discord.js";
 import Command from "../typings/Command";
 
 const help = (client: Client, message: Message, args: Array<string>): void => {
-  return;
+  const helpPage = client.helpPages.get(args[0].toLowerCase());
+
+  if (!helpPage && args[0].toLowerCase()) {
+    message.channel.send("Could not find a help page for that category");
+  } else if (!helpPage && !args[0].toLowerCase()) {
+    message.channel.send(client.helpPages.get("all"));
+  } else {
+    message.channel.send(helpPage);
+  }
 };
 
 module.exports = new Command(
