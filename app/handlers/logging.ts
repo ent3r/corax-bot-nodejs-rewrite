6cf4617ba4e15458f80c1a478d57ac2c43ae8be2
ent3r <32072697+ent3r@users.noreply.github.com>
2020-08-31 22:30:14 +0200

    :mute: Remove file logging
    I don't use it and its not needed

--- app/handlers/logging.ts
+++ app/handlers/logging.ts
@@ -5,17 +5,6 @@ const logger = winston.createLogger({
   // There are two loggers in use. One that writes to the console, and one that writes to a file
   transports: [
     new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.colorize({ all: true }),
-        winston.format.timestamp({ format: "HH:mm:ss" }),
-        winston.format.printf(
-          (info) => `${info.timestamp} - ${info.level}: ${info.message}`
-        )
-      ),
-    }),
-    new winston.transports.File({
-      dirname: "logs",
-      filename: "logging.log",
       level: "debug",
       format: winston.format.combine(
         winston.format.timestamp({ format: "HH:mm:ss" }),
