48d8c91af6c7fe5616cfa28308d22473d4f50c17
ent3r <32072697+ent3r@users.noreply.github.com>
2020-09-07 19:48:01 +0200

    :label: Update typings to get autocompletion

--- app/models/CTFTimeCache.ts
+++ app/models/CTFTimeCache.ts
@@ -2,11 +2,13 @@ import { Schema, model, Document } from "mongoose";
 
 export interface ICTFTimeCache extends Document {
   fetched_at: Date;
+  expires_at: Date,
   data: JSON;
 }
 
-const ctftimeSchema = new Schema({
+const ctftimeSchema = new Schema<ICTFTimeCache>({
   fetched_at: Date,
+  expires_at: Date,
   data: JSON,
 });
 
